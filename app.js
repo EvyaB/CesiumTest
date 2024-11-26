7924d3b007b0f5304cf5d6d1b3e97e57e8cf3698
EvyaB <evyatarbittonwork@gmail.com>
2024-11-26 11:46:20 +0200

    added todo message

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ if (cesiumDiv) {
 }
 
 function DrawCesiumMap() {
+    // TODO - load access token from local configuration instead of hardcoded!
     Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwYzk4NjA1NC0wMTY1LTQ0YTktYTA4Yi0xNmMwMTIxMjljOGIiLCJpZCI6MjU4MDc1LCJpYXQiOjE3MzI2MTA4NzF9.rsJQz0WR9dqkAjsFiRmCTHSHNnPFE8uXfQGnNwIWqyw';
 
     // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
