53f51af63a7584cc84ec07dd8e68aa7ccb7ac198
EvyaB <evyatarbittonwork@gmail.com>
2024-11-26 11:30:41 +0200

    Adding Cesium viewer

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-test = "hello";
-console.log(test);
+const cesiumDiv = document.getElementById("cesiumContainer");
+
+if (cesiumDiv) {
+    DrawCesiumMap();
+} else {
+    console.error("Failed to find correct div for the Cesium Map");
+}
+
+function DrawCesiumMap() {
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwYzk4NjA1NC0wMTY1LTQ0YTktYTA4Yi0xNmMwMTIxMjljOGIiLCJpZCI6MjU4MDc1LCJpYXQiOjE3MzI2MTA4NzF9.rsJQz0WR9dqkAjsFiRmCTHSHNnPFE8uXfQGnNwIWqyw';
+
+    // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+    const viewer = new Cesium.Viewer('cesiumContainer', {
+        terrain: Cesium.Terrain.fromWorldTerrain(),
+        });
+}
